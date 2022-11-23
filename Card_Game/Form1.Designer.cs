bc610a0de5dcba45f727ce338dd9b4b908df9f92
Camilo Jimenez <camiloko1989@gmail.com>
2022-11-22 21:25:22 -0800

    Add all functionality and GUI

---
+++ Card_Game/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Card_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUser1 = new System.Windows.Forms.Button();
+            this.btnUser2 = new System.Windows.Forms.Button();
+            this.btnUser3 = new System.Windows.Forms.Button();
+            this.btnUser4 = new System.Windows.Forms.Button();
+            this.btnPc1 = new System.Windows.Forms.Button();
+            this.btnPc2 = new System.Windows.Forms.Button();
+            this.btnPc3 = new System.Windows.Forms.Button();
+            this.btnPc4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBet = new System.Windows.Forms.Button();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUser1
+            // 
+            this.btnUser1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser1.Location = new System.Drawing.Point(33, 188);
+            this.btnUser1.Name = "btnUser1";
+            this.btnUser1.Size = new System.Drawing.Size(80, 80);
+            this.btnUser1.TabIndex = 0;
+            this.btnUser1.UseVisualStyleBackColor = true;
+            // 
+            // btnUser2
+            // 
+            this.btnUser2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser2.Location = new System.Drawing.Point(151, 188);
+            this.btnUser2.Name = "btnUser2";
+            this.btnUser2.Size = new System.Drawing.Size(80, 80);
+            this.btnUser2.TabIndex = 1;
+            this.btnUser2.UseVisualStyleBackColor = true;
+            // 
+            // btnUser3
+            // 
+            this.btnUser3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser3.Location = new System.Drawing.Point(270, 188);
+            this.btnUser3.Name = "btnUser3";
+            this.btnUser3.Size = new System.Drawing.Size(80, 80);
+            this.btnUser3.TabIndex = 2;
+            this.btnUser3.UseVisualStyleBackColor = true;
+            // 
+            // btnUser4
+            // 
+            this.btnUser4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser4.Location = new System.Drawing.Point(397, 188);
+            this.btnUser4.Name = "btnUser4";
+            this.btnUser4.Size = new System.Drawing.Size(80, 80);
+            this.btnUser4.TabIndex = 3;
+            this.btnUser4.UseVisualStyleBackColor = true;
+            // 
+            // btnPc1
+            // 
+            this.btnPc1.AccessibleDescription = "btnUser1";
+            this.btnPc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPc1.Location = new System.Drawing.Point(33, 40);
+            this.btnPc1.Name = "btnPc1";
+            this.btnPc1.Size = new System.Drawing.Size(80, 80);
+            this.btnPc1.TabIndex = 4;
+            this.btnPc1.UseVisualStyleBackColor = true;
+            // 
+            // btnPc2
+            // 
+            this.btnPc2.AccessibleDescription = "btnUser1";
+            this.btnPc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPc2.Location = new System.Drawing.Point(151, 40);
+            this.btnPc2.Name = "btnPc2";
+            this.btnPc2.Size = new System.Drawing.Size(80, 80);
+            this.btnPc2.TabIndex = 5;
+            this.btnPc2.UseVisualStyleBackColor = true;
+            // 
+            // btnPc3
+            // 
+            this.btnPc3.AccessibleDescription = "btnUser1";
+            this.btnPc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPc3.Location = new System.Drawing.Point(270, 40);
+            this.btnPc3.Name = "btnPc3";
+            this.btnPc3.Size = new System.Drawing.Size(80, 80);
+            this.btnPc3.TabIndex = 6;
+            this.btnPc3.UseVisualStyleBackColor = true;
+            // 
+            // btnPc4
+            // 
+            this.btnPc4.AccessibleDescription = "btnUser1";
+            this.btnPc4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPc4.Location = new System.Drawing.Point(397, 40);
+            this.btnPc4.Name = "btnPc4";
+            this.btnPc4.Size = new System.Drawing.Size(80, 80);
+            this.btnPc4.TabIndex = 7;
+            this.btnPc4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Computer\'s Cards";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "User\'s Cards";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonBet
+            // 
+            this.buttonBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBet.Location = new System.Drawing.Point(33, 322);
+            this.buttonBet.Name = "buttonBet";
+            this.buttonBet.Size = new System.Drawing.Size(99, 40);
+            this.buttonBet.TabIndex = 10;
+            this.buttonBet.Text = "Bet";
+            this.buttonBet.UseVisualStyleBackColor = true;
+            this.buttonBet.Click += new System.EventHandler(this.buttonBet_Click);
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Enabled = false;
+            this.buttonDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDone.Location = new System.Drawing.Point(188, 322);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(99, 40);
+            this.buttonDone.TabIndex = 11;
+            this.buttonDone.Text = "Done";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNew.Location = new System.Drawing.Point(33, 389);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(156, 40);
+            this.buttonNew.TabIndex = 12;
+            this.buttonNew.Text = "New Game";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.buttonBet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPc4);
+            this.Controls.Add(this.btnPc3);
+            this.Controls.Add(this.btnPc2);
+            this.Controls.Add(this.btnPc1);
+            this.Controls.Add(this.btnUser4);
+            this.Controls.Add(this.btnUser3);
+            this.Controls.Add(this.btnUser2);
+            this.Controls.Add(this.btnUser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUser1;
+        private System.Windows.Forms.Button btnUser2;
+        private System.Windows.Forms.Button btnUser3;
+        private System.Windows.Forms.Button btnUser4;
+        private System.Windows.Forms.Button btnPc1;
+        private System.Windows.Forms.Button btnPc2;
+        private System.Windows.Forms.Button btnPc3;
+        private System.Windows.Forms.Button btnPc4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBet;
+        private System.Windows.Forms.Button buttonDone;
+        private System.Windows.Forms.Button buttonNew;
+    }
+}
+
---
+++ Card_Game/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Game
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> userCards = new List<string>() { "0", "0", "0", "0",};
+        List<string> pcCards = new List<string>() { "0", "0", "0", "0", };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBet_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            var tempUser = random.Next(1, 13);
+            var tempPC = random.Next(1, 13);
+
+            if(btnUser1.Text == "") //check the first card available for user
+            {                   
+                if( tempUser <= 10)
+                {
+                    userCards[0] = tempUser.ToString();
+                    btnUser1.Text = tempUser.ToString();
+                }
+                else if(tempUser == 11)
+                {
+                    userCards[0] = "11";
+                    btnUser1.Text = "J";
+                }
+                else if (tempUser == 12)
+                {
+                    userCards[0] = "12";
+                    btnUser1.Text = "Q";
+                }
+                else if (tempUser == 13)
+                {
+                    userCards[0] = "13";
+                    btnUser1.Text = "K";
+                }
+            }
+            else if (btnUser2.Text == "")
+            {
+                if (tempUser <= 10)
+                {
+                    userCards[1] = tempUser.ToString();
+                    btnUser2.Text = tempUser.ToString();
+                }
+                else if (tempUser == 11)
+                {
+                    userCards[1] = "11";
+                    btnUser2.Text = "J";
+                }
+                else if (tempUser == 12)
+                {
+                    userCards[1] = "12";
+                    btnUser2.Text = "Q";
+                }
+                else if (tempUser == 13)
+                {
+                    userCards[1] = "13";
+                    btnUser2.Text = "K";
+                }
+            }
+            else if (btnUser3.Text == "")
+            {
+                if (tempUser <= 10)
+                {
+                    userCards[2] = tempUser.ToString();
+                    btnUser3.Text = tempUser.ToString();
+                }
+                else if (tempUser == 11)
+                {
+                    userCards[2] = "11";
+                    btnUser3.Text = "J";
+                }
+                else if (tempUser == 12)
+                {
+                    userCards[2] = "12";
+                    btnUser3.Text = "Q";
+                }
+                else if (tempUser == 13)
+                {
+                    userCards[2] = "13";
+                    btnUser3.Text = "K";
+                }
+            }
+            else if (btnUser4.Text == "")
+            {
+                if (tempUser <= 10)
+                {
+                    userCards[3] = tempUser.ToString();
+                    btnUser4.Text = tempUser.ToString();
+                }
+                else if (tempUser == 11)
+                {
+                    userCards[3] = "11";
+                    btnUser4.Text = "J";
+                }
+                else if (tempUser == 12)
+                {
+                    userCards[3] = "12";
+                    btnUser4.Text = "Q";
+                }
+                else if (tempUser == 13)
+                {
+                    userCards[3] = "13";
+                    btnUser4.Text = "K";
+                }
+                buttonBet.Enabled = false;
+                
+            }
+            else
+            {
+                buttonBet.Enabled = false;
+            }
+
+            if (btnPc1.Text == "") //check the first card available for computer
+            {
+                if (tempPC <= 10)
+                {
+                    pcCards[0] = tempPC.ToString();
+                    btnPc1.Text = tempPC.ToString();
+                }
+                else if (tempPC == 11)
+                {
+                    pcCards[0] = "11";
+                    btnPc1.Text = "J";
+                }
+                else if (tempPC == 12)
+                {
+                    pcCards[0] = "12";
+                    btnPc1.Text = "Q";
+                }
+                else if (tempPC == 13)
+                {
+                    pcCards[0] = "13";
+                    btnPc1.Text = "K";
+                }
+            }
+            else if (btnPc2.Text == "")
+            {
+                if (tempPC <= 10)
+                {
+                    pcCards[1] = tempPC.ToString();
+                    btnPc2.Text = tempPC.ToString();
+                }
+                else if (tempPC == 11)
+                {
+                    pcCards[1] = "11";
+                    btnPc2.Text = "J";
+                }
+                else if (tempPC == 12)
+                {
+                    pcCards[1] = "12";
+                    btnPc2.Text = "Q";
+                }
+                else if (tempPC == 13)
+                {
+                    pcCards[1] = "13";
+                    btnPc2.Text = "K";
+                }
+            }
+            else if (btnPc3.Text == "")
+            {
+                if (tempPC <= 10)
+                {
+                    pcCards[2] = tempPC.ToString();
+                    btnPc3.Text = tempPC.ToString();
+                }
+                else if (tempPC == 11)
+                {
+                    pcCards[2] = "11";
+                    btnPc3.Text = "J";
+                }
+                else if (tempPC == 12)
+                {
+                    pcCards[2] = "12";
+                    btnPc3.Text = "Q";
+                }
+                else if (tempPC == 13)
+                {
+                    pcCards[2] = "13";
+                    btnPc3.Text = "K";
+                }
+            }
+            else if (btnPc4.Text == "")
+            {
+                if (tempPC <= 10)
+                {
+                    pcCards[3] = tempPC.ToString();
+                    btnPc4.Text = tempPC.ToString();
+                }
+                else if (tempPC == 11)
+                {
+                    pcCards[3] = "11";
+                    btnPc4.Text = "J";
+                }
+                else if (tempPC == 12)
+                {
+                    pcCards[3] = "12";
+                    btnPc4.Text = "Q";
+                }
+                else if (tempPC == 13)
+                {
+                    pcCards[3] = "13";
+                    btnPc4.Text = "K";
+                }
+                buttonBet.Enabled = false;
+                CalculateResults();  //Calculate result when all cards were played
+            }
+            else
+            {
+                
+                buttonBet.Enabled = false;
+                
+            }
+
+            buttonDone.Enabled = true;
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            //list go back to original state
+            for(int i=0 ; i<4; i++)
+            {
+                pcCards[i] = "0";
+                userCards[i] = "0";
+            }
+
+            //all buttons are empty again
+            btnPc1.Text = "";
+            btnPc2.Text = "";
+            btnPc3.Text = "";
+            btnPc4.Text = "";
+
+            btnUser1.Text = "";
+            btnUser2.Text = "";
+            btnUser3.Text = "";
+            btnUser4.Text = "";
+
+            buttonBet.Enabled = true;  //botton for Bet becomes available
+
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            CalculateResults();
+
+
+
+
+        }
+
+
+        private void CalculateResults()
+        {
+            double userResult = Results(userCards);
+            double PcResult = Results(pcCards);
+            string blastPc = "";
+            string blastUser = "";
+
+            if (userResult <= 10)
+            {
+                blastUser = "You Blast! ";
+            }
+
+            if (PcResult <= 10)
+            {
+                blastPc = "You Blast! ";
+            }
+
+            if (PcResult <= 10 && userResult <= 10)
+            {
+                MessageBox.Show("Both Blast, Computer Wins! ");
+            }
+            else if (userResult > PcResult)
+            {
+                MessageBox.Show(blastUser + "User Wins! " + userResult.ToString());
+            }
+            else if (userResult < PcResult)
+            {
+                MessageBox.Show(blastPc + "Computer Wins! " + PcResult.ToString());
+            }
+            else if (PcResult > 10 && userResult > 10)
+            {
+                MessageBox.Show("No one blast, User Wins! " + userResult.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Draw! " + PcResult.ToString());
+            }
+        }
+        private double Results(List<string> userCards)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (double.Parse(userCards[i]) > 10)
+                {
+                    sum = sum + 0.5;
+                }
+                else
+                {
+                    sum = sum + double.Parse(userCards[i]);
+                }
+                
+            }
+            return sum;
+        }
+
+        
+    }
+}
